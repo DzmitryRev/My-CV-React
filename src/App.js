15d8b37a1e3355c1662da6f3dbd05f86ab7d290c
unknown <revchenkodzmitry@gmail.com>
2022-05-27 20:27:31 +0300

    feat: install and set up i18n (Fri, May 27, 2022 08:27:31 PM)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "./scss/main.scss";
+import { Route, Routes } from "react-router-dom";
 import { Header, Footer, Sidebar } from "./components";
+import { Main } from "./pages";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
+  //   const { t } = useTranslation();
+  //   return (
+  //     <>
+  //       <h1>{t("title", { ns: "header" })}</h1>
+  //     </>
+  //   );
   return (
     <div>
       <Header />
       <div>
         <Sidebar />
+        <Routes>
+          <Route path="/">
+            <Route index element={<Main />} />
+          </Route>
+        </Routes>
       </div>
       <Footer />
     </div>
